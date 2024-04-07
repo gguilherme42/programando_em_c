c787b4c7178ce74ad7a3e34a0d4a42c2bb447385
gguilherme42 <anoobs@tutamail.com>
2024-04-07 15:47:08 -0300

    Finalizado ex004

--- main/grosa/ex004/ex004.c
+++ main/grosa/ex004/ex004.c
@@ -1,11 +1,11 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 
 float my_sum(float n1, float n2){
 	return n1 + n2;
 }
 
-float function my_subtraction(float n1, float n2){
+float my_subtraction(float n1, float n2){
 	return n1 - n2;
 }
 
@@ -19,43 +19,55 @@ float my_division(float n1, float n2){
 }
 
 
-int main(){
-	float number1, number2;
-	int user_choice;	
+void calculator(int choice, float n1, float n2){
 
+	if (choice > 0 && choice < 5) {
+		if (choice == 1) {
+			printf("A soma de %.2f e %.2f é %.2f\n", n1, n2, my_sum(n1, n2));
+		}
+		else if (choice == 2) {
+		
+			printf("A subtração de %.2f e %.2f é %.2f\n", n1, n2, my_subtraction(n1, n2));
+		}
+		else if (choice == 3) {
+			printf("A multiplicação de %.2f e %.2f é %.2f\n", n1, n2, my_multiplication(n1, n2));
+		}
+		else if (choice == 4) {
+		
+			printf("A divisão de %.2f e %.2f é %.2f\n", n1, n2, my_division(n1, n2));
+		}
+	} else {
+	
+		printf("Saindo.....");
+	}
+}
+
+
+void menu(){
 	printf("-------------- MENU -----------------\n");
 	printf("1 - ADIÇÃO\n");
 	printf("2 - SUBTRAÇÃO\n");
 	printf("3 - MULTIPLICAÇÃO\n");
 	printf("4 - DIVISÃO\n");
 	printf("0 - SAIR \n");
+	printf("-------------------------------------\n");
+}
+
+
+int main(){
+	float number1, number2;
+	int user_choice;	
+	
+	menu();
 	scanf("%d", &user_choice);
 
 	printf("Digite o primeiro número: \n");
 	scanf("%f", &number1);
 	printf("Digite o segundo número: \n");
 	scanf("%f", &number2);
-	printf("-------------------------------------\n");
 
-	if (user_choice > 0 && user_choice < 5) {
-		if (user_choice == 1) {
-			printf("A soma de %f e %f é %f", number1, number2, my_sum(number1, number2));
-		}
-		else if (user_choice == 2) {
-		
-			printf("A subtração de %f e %f é %f", number1, number2, my_subtraction(number1, number2));
-		}
-		else if (user_choice == 3) {
-			printf("A multiplicação de %f e %f é %f", number1, number2, my_multiplication(number1, number2));
-		}
-		else if (user_choice == 4) {
-		
-			printf("A divisão de %f e %f é %f", number1, number2, my_division(number1, number2));
-		}
-	} else {
-	
-		printf("Saindo.....");
-	}
+	calculator(user_choice, number1, number2);
+
 
 	return 0;
 }
